643a53e3b1d6c86a7802fe1196b703d48a4e3125
GNUOverYooouu <dtortoise99@gmail.com>
2021-06-02 12:25:15 -0700

    The bot itself

---
+++ main.ts
@@ -0,0 +1,13 @@
+const commands = new discord.command.CommandGroup({
+  defaultPrefix: '!' // Use !<commandname>
+});
+
+commands.raw('help', (message) =>
+  message.reply('Commands: help, hi, github. Use ! to use them.')
+);
+
+commands.raw('hi', (message) => message.reply('Hi IG'));
+
+commands.raw('github', (message) =>
+  message.reply('https://github.com/Platypus-Tech/new-platypus-os-drafts')
+);
